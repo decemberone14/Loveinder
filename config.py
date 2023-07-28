d4791986b1326b73818b57daf7d6ea123efcef1b
decemberone14 <122971058+decemberone14@users.noreply.github.com>
2023-07-28 23:21:06 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,10 @@
+# config.py
+
+# Токен вашего сообщества ВКонтакте
+community_token = 'токен'
+
+# Токен доступа для работы с API ВКонтакте
+access_token = 'токен'
+
+# Строка подключения к базе данных PostgreSQL
+db_url_object = "postgresql://postgres:postgres@localhost:5432/netologydip"
